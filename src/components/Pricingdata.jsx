d20f67a5394caace8c1bbd1fbcb605a8292c4d39
sumittale5654 <sumittale2001@gmaill.com>
2023-10-03 22:24:03 +0530

    image add

--- src/components/Pricingdata.jsx
+++ src/components/Pricingdata.jsx
@@ -1,6 +1,9 @@
 import p10 from "../assets/p10.png";
 import p11 from "../assets/p11.png";
 import p221 from "../assets/p221.jpg";
+import p14 from "../assets/p14.jpg";
+import p15 from "../assets/p15.jpg";
+import p16 from "../assets/p16.jpeg";
 
 
 
@@ -21,6 +24,23 @@ const Projectcarddata = [
         imgsrc : p221,
         title : "TCS",
         text: "Completing the course on develope soft skill. ",
+       
+    },
+    {
+        imgsrc : p14,
+        title : "TCS ",
+        text: "Completing the course on Make Impactful Presentations",
+    },
+    {
+        imgsrc : p15,
+        title : "Conferences of Emerging Technology",
+        text: "Participation & Presentation of the paper title Comparative study of mostly used stacks of web technology in e-Conference on Data Science and Intelligent Computing. ",
+       
+    },
+    {
+        imgsrc : p16,
+        title : "Certificate of Paper Publication",
+        text: "MY TUTOR APPLICATION USING MERN Has been published in GIS SCIENCE JOURNAL Volume 10, Issue 5, may 2023. ",
        
     },
 
